8d2a27a9935e60def24f60c456ac1b87f169a7ea
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-12 21:00:37 -0300

    feat(migrations): create meals   A new meal migration has been created in the database. It has a relationship with the user, meaning each user will have their own meal.

---
+++ db/migrations/20260312234738_meals.ts
@@ -0,0 +1,18 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('id').primary().defaultTo(knex.fn.uuid())
+    table.uuid('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE')
+    table.text('name').notNullable()
+    table.text('description').notNullable()
+    table.boolean('is_in_diet').notNullable()
+    table.timestamp('date').notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meals')
+}
